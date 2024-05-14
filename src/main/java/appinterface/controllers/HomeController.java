3c6b3884b17a8915676d9fd5156a4c39515e646b
VitalyProlomov <vitalyp3409@gmail.com>
2024-05-15 00:21:10 +0300

    15.05 0:20 ---------------------------------- - Added Help Button - App is now fully functional ---------------------------------

--- src/main/java/appinterface/controllers/HomeController.java
+++ src/main/java/appinterface/controllers/HomeController.java
@@ -1,4 +1,5 @@
 package appinterface.controllers;
+
 import appinterface.PSAApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.fxml.FXML;
@@ -45,6 +46,7 @@ public class HomeController {
     @FXML
     private Button deleteGamesButton;
 
+    //    private final static String SERIALIZED_GAMES_PATH = "resources/serializedFiles/serializedGames.txt";
     private final static String SERIALIZED_GAMES_PATH = "src/main/resources/serializedFiles/serializedGames.txt";
     private GamesSet gamesSet;
 
@@ -223,19 +225,37 @@ public class HomeController {
     private void onHelpButtonClicked() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText("""
-                Hello, user. Poker Statistics Analyzer is an app 
+                Hello, user. Poker Statistics Analyzer is an app
                 that will help you improve your strategy by
                 analyzing your games.
-                There are following functions here: 
+                There are following functions here:
                 1) Downloaded games - here yu can see all
-                of the games you uploaded
+                of the games you uploaded and replay them by
+                double clicking on row with chosen game.
+                2) Upload games - upload new games (format
+                must be from the files downloaded on website PokerCraft.com
+                Website can only be accessed through lin in poker room GGPoker.
+                3) Examine Players - assign player nicknames to their
+                hashes. Players can then be seen assigned to the games.
+                4) EV Calculator - here you can calculate mathematical
+                expected value with chosen hands on chosen board.
+                5) Hands EV - here you can see all the expected value
+                with every possible hand.
+                6) Enhanced Stats - here you can analyze your actions
+                and some statistics in games that you can ilter by
+                different parameters
+                7) Delete games - delete unwanted games (can be searched by filter)
+                Have fun using the app!
                 """);
+        alert.show();
     }
 
     private void initializeSerializedSavedGames() {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
 
+//            HomeController.class.getResourceAsStream(SERIALIZED_GAMES_PATH);
+//            File file = new File(SERIALIZED_GAMES_PATH);
             if (new File(SERIALIZED_GAMES_PATH).length() != 0) {
                 gamesSet = objectMapper.readValue(new File(SERIALIZED_GAMES_PATH), GamesSet.class);
             }
@@ -262,7 +282,7 @@ public class HomeController {
             objectMapper.writeValue(file, gamesSet);
         } catch (Exception exception) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setContentText("File with saved games was not able to upload, please close it if it is opened");
+            alert.setContentText("File with saved games was not able to upload, please close it, if it is opened");
             alert.show();
         }
     }
